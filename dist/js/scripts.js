e4128a98869ba47ed9a204ef60cd81cf1847fd31
Ingo Steinke <ingosteinke@gmx.de>
2022-08-23 19:50:29 +0200

    add animations

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,7 +6,12 @@
 function intersectionCallback(intersectingEntries, observer) {
   for (var j = 0; j < intersectingEntries.length; j++) {
     /* var txt = entry.target.id + " visibility: " + entry.isIntersecting; */
-    entry.classList.add('animate__animated');
+    console.log('add animation class to intersecting entry ' + j, intersectingEntries[j]);
+    if (intersectingEntries[j].target && intersectingEntries[j].target.classList) {
+      intersectingEntries[j].target.classList.add('animate__animated');
+      console.log('added animated class to intersectingEntries[j].target');
+      console.log(intersectingEntries[j].target);
+    }
   }
 }
 
@@ -18,8 +23,10 @@ var options = {
 
 document.addEventListener('DOMContentLoaded', function() {
   var observer = new IntersectionObserver(intersectionCallback, options);
-  var elementsAnimatedOnVisibility = document.getElementsByClassName("animated-on-visibility");
+  console.log('initialize intersection observer ...');
+  var elementsAnimatedOnVisibility = document.getElementsByClassName("animate--on-visibility");
   for (var i = 0; i < elementsAnimatedOnVisibility.length; i++) {
+    console.log('must observer element ' + i, elementsAnimatedOnVisibility[i]);
     observer.observe(elementsAnimatedOnVisibility[i]);
   }
 });
