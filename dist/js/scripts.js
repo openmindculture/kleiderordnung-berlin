29b55611e33b1175d55e27a9d813df0f28eecdd6
Ingo Steinke <ingosteinke@gmx.de>
2022-09-10 18:39:48 +0200

    trying to configure observer for animations

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,29 +1,36 @@
 'use strict';
 
+/** @type {String} */
+var animatingClassName = 'animate__animated';
+
 /**
  * Observe .animated-on-visibility adding animation class when they become visible
  * @param {Array} intersectingEntries
  * @param {IntersectionObserver=} observer
  */
 function intersectionCallback(intersectingEntries) {
+  console.log('intersection has happened');
+  // ^ fucking bullshit, why does this always trigger immediately on page load already?!
   for (var j = 0; j < intersectingEntries.length; j++) {
-    /* var txt = entry.target.id + " visibility: " + entry.isIntersecting; */
     console.log('add animation class to intersecting entry ' + j, intersectingEntries[j]);
     if (intersectingEntries[j].target && intersectingEntries[j].target.classList) {
-      intersectingEntries[j].target.classList.add('animate__animated');
-      console.log('added animated class to intersectingEntries[j].target');
+      var animationClassName = intersectingEntries[j].target.dataset.animationclass;
+      intersectingEntries[j].target.classList.add(animatingClassName, animationClassName);
+      console.log(`added animated class names to intersectingEntries[j].target: animatingClassName: ${animatingClassName}, animationClassName: ${animationClassName}`);
       console.log(intersectingEntries[j].target);
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  const observerRoot = document;
+  console.log('using observerRoot', observerRoot);
   var observer = new IntersectionObserver(intersectionCallback, {
-    root: null,
+    root: observerRoot,
     rootMargin: '0px',
-    threshold: 0.3
+    threshold: 0.9
   });
-  console.log('initialize intersection observer ...');
+  console.log('initialize intersection observer (0px, 0.9 threshold)');
   var elementsAnimatedOnVisibility = document.getElementsByClassName("animate--on-visibility");
   for (var i = 0; i < elementsAnimatedOnVisibility.length; i++) {
     console.log('must observer element ' + i, elementsAnimatedOnVisibility[i]);
