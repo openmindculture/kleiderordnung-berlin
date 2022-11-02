73a987a2772ed7019b2c9b62d11d7f442c6b1cb1
Ingo Steinke <ingosteinke@gmx.de>
2022-11-02 15:18:39 +0100

    simpler consent work in progress

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -40,6 +40,7 @@ function intersectionCallback(intersectingEntries) {
   for (var j = 0; j < intersectingEntries.length; j++) {
     if (intersectingEntries[j].isIntersecting && intersectingEntries[j].intersectionRatio > observerOptions.threshold) {
       if (intersectingEntries[j].target && intersectingEntries[j].target.classList) {
+        // switch animation or insta feed prepareExternalFeed(intersectingEntries[j])
         var datakey = 'animationclass';
         var flexContainer = intersectingEntries[j].target.parentElement;
         if (flexContainer) {
@@ -55,10 +56,71 @@ function intersectionCallback(intersectingEntries) {
   }
 }
 
+// callback insta feed visible:
+// mit Visibility prüfen wir ob es schon Consent gibt
+//   consent && load external scripts
+//   else add event handlers to button to set cookies and load external scripts
+
+function prepareExternalFeed(buttonWrapperElement) {
+  if (document.cookie.split(';').some((item) => item.trim().startsWith('reader='))) {
+    allowAndActivateExternalFeed();
+  } else {
+    // add button handlers
+    buttonWrapperElement.querySelectorAll("[data-allow]");
+    // onclick = function(buttonElement) { allowAndActivateExternalFeed(buttonElement)
+  }
+}
+
+// callback allowAndLoad(allowanceDuration)
+
+function allowAndActivateExternalFeed(buttonElement) {
+  var feedContainerElement = buttonElement.closest(".feed__container");
+  var consentCookie = "instafeed=allow; SameSite=strict; secure";
+  if (buttonElement.dataset.allow === "always") {
+    // ;max-age=max-age-in-seconds (e.g., 60*60*24*365 or 31536000 for a year)
+    consentCookie += "expires=Fri, 31 Dec 9999 23:59:59 GMT;";
+  } // If neither expires nor max-age specified it will expire at the end of session.
+  document.cookie = consentCookie;
+  if (feedContainerElement) {
+    activateExternalFeed(feedContainerElement);
+  }
+}
+
+function activateExternalFeed(feedContainerElement) {
+  if (!feedContainerElement) { return; }
+  var styleFileUrl = feedContainerElement.dataset.styleurl;
+  if (styleFileUrl) {
+    fetch(styleFileUrl)
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(text) {
+        var style = document.head.appendChild(document.createElement('style'));
+        style.textContent = text;
+        console.log("juicerFeedContainer", feedContainerElement);
+        if (feedContainerElement && feedContainerElement.classList) {
+          feedContainerElement.classList.remove("initially-hidden");
+        }
+      }).catch(function(err) {
+      console.error('style load failed with error', err);
+    });
+  }
+  var scriptFileUrl = feedContainerElement.dataset.scripturl;
+  if (scriptFileUrl) {
+    var scriptElement = document.createElement('script')
+    scriptElement.src = scriptFileUrl;
+    document.head.append(scriptElement);
+  }
+  feedContainerElement.classList.add('feed__container--active');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  var observer = new IntersectionObserver(intersectionCallback, observerOptions);
+  var elementsActivatedOnVisibilityAndConsent = {};
+  // ohne visibility passiert nichts mit dem Markup
+
   var prefersReducedMotionQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
   if (prefersReducedMotionQuery && !prefersReducedMotionQuery.matches) {
-    var observer = new IntersectionObserver(intersectionCallback, observerOptions);
     var elementsAnimatedOnVisibility = document.getElementsByClassName("animate--on-visibility");
     for (var i = 0; i < elementsAnimatedOnVisibility.length; i++) {
       observer.observe(elementsAnimatedOnVisibility[i]);
