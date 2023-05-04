77fef75be12b0225896dfdb260dd776b10b3d03a
Ingo Steinke <ingosteinke@gmx.de>
2023-05-04 18:55:13 +0200

    use more classic demo markup

--- src/functions.php
+++ src/functions.php
@@ -3,3 +3,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // TODO dequeue third party styles like contact form 7
+// dequeue svg filters above page header
+// if still necessary:
+// remove_action( 'wp_head', 'wp_generator' ); // goes into functions.php
+// remove_action( 'wp_head', 'wp_generator' );
