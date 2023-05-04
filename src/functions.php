0cb6d663f0f72a2fbd11a8991df4f6c36dcbc47d
Ingo Steinke <ingosteinke@gmx.de>
2023-05-04 23:20:53 +0200

    modularize and declutter

--- src/functions.php
+++ src/functions.php
@@ -10,10 +10,21 @@ if ( ! defined ( 'KLEIDERORDNUNG_URI' ) ){
   define( 'KLEIDERORDNUNG_URI', get_template_directory_uri() );
 }
 
-define('DISABLE_FSE', true); // disable site editing features like editable template parts
+// the following should go to wp-config.php
+define('DISABLE_FSE', true); // disable site editing features like editable template parts TODO not working here
 
 // TODO dequeue third party styles like contact form 7
+
 // dequeue svg filters above page header
+add_action( 'wp_enqueue_scripts', function() {
+  wp_dequeue_style( 'global-styles' );
+} );
+
+add_action('after_setup_theme', function() {
+  remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+  remove_action( 'in_admin_header', 'wp_global_styles_render_svg_filters' );
+}, 10, 0);
+
 // if still necessary:
 // remove_action( 'wp_head', 'wp_generator' ); // goes into functions.php
 // remove_action( 'wp_head', 'wp_generator' );
