6bacd8c01f3fe77d964c773254709af6c6a326ff
Ingo Steinke <ingosteinke@gmx.de>
2023-05-15 07:37:08 +0200

    target version 2.1.7

--- src/functions.php
+++ src/functions.php
@@ -11,7 +11,7 @@ if ( ! defined ( 'KLEIDERORDNUNG_URI' ) ){
 }
 
 if ( ! defined ( 'KLEIDERORDNUNG_THEME_VERSION' ) ){
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.1.6' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.1.7' );
 }
 
 // the following should go to wp-config.php
