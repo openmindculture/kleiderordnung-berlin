86a1e7284b401095121c6c6e03cbcbae60113e5e
Ingo Steinke <ingosteinke@gmx.de>
2023-05-19 14:39:19 +0200

    contact form section

--- src/index.php
+++ src/index.php
@@ -657,21 +657,18 @@
     </div>
     <div class="contact__columns__column-wrapper contact__columns__column-wrapper--lg">
 
-      <div class="contact__columns__column contact__columns__column-50">
+      <div class="contact__columns__column contact__columns__column--50">
         <!-- BEGIN vcard imprint contact information - TODO: update using rel=me etc. -->
         <div class="contact__calltoactiontext contact__calltoactiontext--primary">Habe ich Dich neugierig gemacht oder hast du Fragen? Melde dich gern bei mir!</div>
         <div class="contact__vcard">
-
-          <address class="vcard contrast--varies contrast--more">
-               <div class="" itemprop="company">Kleiderordnung</div>
-              <div class=fn itemprop=name>Tina Steinke</div>
-              <div class="role jobtitle" itemprop="jobTitle">
-                Personal sustainable Stylist</div>
-
-              <div>Tel. <a class=telephone itemprop=telephone href=tel:+491636823820><span class=visually-spaced>0</span><span class="visually-spaced dot-after">163</span><span class=visually-spaced>682</span><span class=visually-spaced>382</span><span class=visually-spaced>0</span></a>
-              </div>
-            <div>
-              <a class=email itemprop=email itemscope href=mailto:martinasteinke@posteo.de>martinasteinke@posteo.de</a>
+          <address class="vcard contact__vcard__address">
+            <div class="contact__vcard__company" itemprop="company">Kleiderordnung</div>
+            <div class="contact__vcard__fullname fn" itemprop="name">Tina Steinke</div>
+            <div class="contact__vcard__jobtitle jobtitle role" itemprop="jobTitle">Personal sustainable Stylist</div>
+            <div class="contact__vcard__phone">Tel. <a class=telephone itemprop=telephone href=tel:+491636823820><span class=visually-spaced>0</span><span class="visually-spaced dot-after">163</span><span class=visually-spaced>682</span><span class=visually-spaced>382</span><span class=visually-spaced>0</span></a>
+            </div>
+            <div class="contact__vcard__email">
+              <a class="contact__vcard__email__mailto email" itemprop="email" itemscope href=mailto:martinasteinke@posteo.de>martinasteinke@posteo.de</a>
             </div>
           </address>
           <div class="contact__calltoactiontext contact__calltoactiontext--secondary">
@@ -686,7 +683,7 @@
         <!-- END vcard imprint contact information -->
       </div>
 
-      <div class="contact__columns__column contact__columns__column-50">
+      <div class="contact__columns__column contact__columns__column--50">
         <!-- contact 7 form using fixed shortcode (TODO: dynmic first available by language? -->
         <?php echo do_shortcode( '[contact-form-7 id="16" title="Kontaktformular deutsch"]' ); ?>
 
@@ -697,11 +694,11 @@
 
 <footer id="footer" class="site-footer">
   <div class="footer__columns__column-wrapper">
-    <div class="footer__columns__column footer__columns__column-50">
+    <div class="footer__columns__column footer__columns__column--50">
       2023 © Kleiderordnung Martina Steinke  – All rights reserved
       Impressum Datenschutz
     </div>
-    <div class="footer__columns__column footer__columns__column-50">
+    <div class="footer__columns__column footer__columns__column--50">
       Mitglied bei
       <a href=https://www.sustainable-stylists.com/tina-steinke target="_blank" rel="me noopener">
         <img
