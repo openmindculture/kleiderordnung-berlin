e5e4a28d7d1302bba0a218c1e48803f2ebdab65c
Ingo Steinke <ingosteinke@gmx.de>
2023-05-25 15:40:44 +0200

    add accessible slides fallbacks

--- src/inc/structure/stories.php
+++ src/inc/structure/stories.php
@@ -12,7 +12,7 @@
     <div class="stories__viewport carousel__viewport">
 
       <!-- Story 1 -->
-      <div class="stories__story carousel__item">
+      <div class="stories__story carousel__item" id="stories-items-first">
         <figure class="stories__story__image">
           <img
             src="<?php echo get_template_directory_uri() ?>/img/maxi.jpg" width="319" height="567"
@@ -72,7 +72,7 @@
       </div>
 
       <!-- Story 4 - absichtlich ohne Bild -->
-      <div class="stories__story carousel__item">
+      <div class="stories__story carousel__item" id="stories-items-last">
         <!-- einige Testimonials haben eventuell kein Bild -->
         <figure class="stories__story__quote">
           <figcaption class="stories__story__quote__author">
@@ -87,7 +87,7 @@
   </div>
 
   <nav class="stories__navigation carousel__navigation">
-    <a class="stories__navigation__link stories__navigation__link--prev carousel__navigation__prev" href="#prev">
+    <a class="stories__navigation__link stories__navigation__link--prev carousel__navigation__prev" href="#stories-items-first">
       <svg width="70" height="42" viewBox="0 0 70 42" fill="none">
         <title>zurück</title>
         <path d="M-1.36497e-06 21C11.598 21 21 11.598 21 0" stroke="currentColor" stroke-width="2"/>
@@ -95,7 +95,7 @@
         <line y1="21" x2="70" y2="21" stroke="currentColor" stroke-width="2"/>
       </svg>
     </a>
-    <a class="stories__navigation__link stories__navigation__link--next carousel__navigation__next" href="#next">
+    <a class="stories__navigation__link stories__navigation__link--next carousel__navigation__next" href="#stories-items-last">
       <svg width="70" height="42" viewBox="0 0 70 42" fill="none">
         <title>vor</title>
         <path d="M70 21C58.402 21 49 30.402 49 42" stroke="currentColor" stroke-width="2"/>
