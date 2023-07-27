b09fdd3ea1af015b8da3fcb4a984c3d9aef1d077
Ingo Steinke <ingosteinke@gmx.de>
2023-07-27 21:55:11 +0200

    fix CTA layout and content

--- src/inc/structure/calltoaction.php
+++ src/inc/structure/calltoaction.php
@@ -8,7 +8,7 @@
 ?>
 <div class="calltoaction">
   <div class="calltoaction__text">
-    <?php echo get_field('page_contact_teaser_text', KLEIDERORDNUNG_FRONT_PAGE_ID) ?>
+    <?php echo get_field('page_cta_text', KLEIDERORDNUNG_FRONT_PAGE_ID) ?>
   </div>
   <div class="calltoaction__buttonwrapper">
     <a class="button button--primary" href="#kontakt" tabindex="0">
