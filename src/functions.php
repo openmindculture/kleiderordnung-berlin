c4ae2554287179566aefd9508e0edf24f9a0c834
Ingo Steinke <ingosteinke@gmx.de>
2023-07-27 21:35:05 +0200

    target version

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.5.4' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.5.5' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
