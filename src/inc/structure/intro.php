b4d6daa51c741d00c7d1408c83ffb2e72a02a413
Ingo Steinke <ingosteinke@gmx.de>
2023-08-08 18:11:31 +0200

    prevent no-js version intro flashing

--- src/inc/structure/intro.php
+++ src/inc/structure/intro.php
@@ -28,6 +28,18 @@
         alt="<?php _e( 'Portrait von Tina Steinke', 'kleiderordnung' ) ?>"
       >
     </div>
+    <!-- equivalent fallback for missing javascript -->
+    <noscript>
+      <div class="intro__keyvisual__animation--noscript-fallback">
+        <img
+          src="<?php echo get_template_directory_uri() ?>/img/keyvisual-kleiderordnung.jpg" width="1160" height="581"
+          srcset="<?php echo get_template_directory_uri() ?>/img/keyvisual-kleiderordnung.jpg 1x, <?php echo get_template_directory_uri() ?>/img/keyvisual-kleiderordnung@2x.jpg 2x"
+          loading="lazy"
+          alt="<?php _e( 'Portrait von Tina Steinke', 'kleiderordnung' ) ?>"
+        >
+      </div>
+      </div>
+    </noscript>
   </div>
 
   <div class="intro__keytext">
