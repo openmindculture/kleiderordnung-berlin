5753310f5ba513ca13b41b6757e14bb7baa9b88a
Ingo Steinke <ingosteinke@gmx.de>
2023-08-08 13:06:48 +0200

    test coverage (WIP)

--- codecept/homepage_test.js
+++ codecept/homepage_test.js
@@ -6,6 +6,19 @@ Scenario('Test website content and navigation', ({ I }) => {
   // I.see('Mode');
 
   /* TODO
+
+
+    I click on top navi link DE
+    I see a certain German text
+
+        */
+  I.saveScreenshot('Homepage_Screenshot.png');
+  I.seeVisualDiff('Homepage_Screenshot.png', {
+    tolerance: 2,
+    prepareBaseImage: false
+  });
+
+  /*
     I see the page.
     I see text rendered in one of the expected font families.
     I scroll down and see animated feature boxes.
@@ -13,11 +26,34 @@ Scenario('Test website content and navigation', ({ I }) => {
     I click on the allow buttton and wait for instagram content to appear.
     I verify all outgoing links.
     I verify the contact phone number.
+    I verify the contact email.
+
+    I click on bottom navi link Impressum
+    I see headline "Impressum"
+    I see Donaustr.
+    I save screenshot "Impressum_Screenshot"
+
+    I click on top navi link EN
+    I see headline "Imprint"
+    I see Donaustr.
+
+    I click on bottom navi link Privacy
+    I see headline "Privacy"
+    I clock on top navi link DE
+    I see headline "Datenschutzerklärung"
+    I save screenshot "Datenschutzerklärung"
+
+    I clock on bottom navi link AGB
+    I see headline "Allgemeine Geschäftsbedingungen"
+    I see sub headline "Schlussbestimmungen"
+    I save screenshot "AGB"
+    I click on top navi link EN
+    I see title "Terms of Service"
+    I see something from the end of the TOS
+
+    I clock on top navi logo link
+    I see intro key visual
    */
 
-  I.saveScreenshot('Homepage_Screenshot.png');
-  I.seeVisualDiff('Homepage_Screenshot.png', {
-    tolerance: 2,
-    prepareBaseImage: false
-  });
+
 });
