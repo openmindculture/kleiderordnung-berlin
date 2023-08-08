850cadfd908c54f273001e6aa0120bc579dabd86
Ingo Steinke <ingosteinke@gmx.de>
2023-08-08 15:14:12 +0200

    refactor sticky header logic

--- src/js/inc/observeStickyHeader.js
+++ src/js/inc/observeStickyHeader.js
@@ -1,15 +1,30 @@
 /** Header / Navigation control */
-export function kleiderordnung_observeStickyHeader() {
-    if (!window.kleiderordnung.config.supportsIntersectionObserver) { return; }
-    var stickyHeader = document.getElementById(window.kleiderordnung.config.stickyHeaderId);
-    if (!stickyHeader) { return; } /* TODO refactor string to config */
-    window.kleiderordnung.state.rootElement.style.setProperty('--header-height', '' + stickyHeader.offsetHeight + 'px');
+/** @param {object} config configuration object
+ *  @property {boolean} supportsIntersectionObserver
+ *  @property {object} observerOptions
+ *  @property {string} stickyHeaderId e.g. 'site-header'
+ *  @property {string} stickyHeaderHeightProperty e.g.'--header-height'
+ *  @property {string} stickyStuckHeaderHeightProperty e.g. '--header-height--stuck'
+ *  @property {string} socialMediaSectionId e.g. 'socialmedia'
+ *
+ *  @property {string} introKeyvisualAnimationId e.g. 'intro-keyvisual-animation'
+ *  @property {string} introKeyvisualMousetrapId e.g. 'intro-keyvisual-mousetrap'
+ *  @property {string} introKeyvisualScriptId e.g. 'lottie-player-script'
+ *  @property {string} introKeyvisualComponentTagName e.g. 'lottie-player'
+ *  @property {string} noGlobalThisClassName e.g. 'no-globalthis'
+ *  @property {number} introKeyvisualTimeoutMilliseconds 30000
+ */
+export function kleiderordnung_observeStickyHeader(config) {
+    if (!config.supportsIntersectionObserver) { return; }
+    var stickyHeader = document.getElementById(config.stickyHeaderId);
+    if (!stickyHeader) { return; }
+    window.kleiderordnung.state.rootElement.style.setProperty(config.stickyHeaderHeightProperty, '' + stickyHeader.offsetHeight + 'px');
     window.kleiderordnung.observers.stickyObserver = new IntersectionObserver(
         function (intersectingEntries) {
             window.kleiderordnung.state.isHeaderStuck = intersectingEntries[0].intersectionRatio < 1;
             intersectingEntries[0].target.classList.toggle(window.kleiderordnung.config.stuckClassName, window.kleiderordnung.state.isHeaderStuck);
-            if (window.kleiderordnung.state.isHeaderStuck) { /* TODO refactor string to config */
-                window.kleiderordnung.state.rootElement.style.setProperty('--header-height--stuck', '' + stickyHeader.offsetHeight + 'px');
+            if (window.kleiderordnung.state.isHeaderStuck) {
+                window.kleiderordnung.state.rootElement.style.setProperty(config.stickyStuckHeaderHeightProperty, '' + stickyHeader.offsetHeight + 'px');
             }
         },
         {threshold: [1]}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,101 +16,97 @@ import {kleiderordnung_observeWaypointAnchors} from './inc/observeWaypointAnchor
 
 /* global document, window */
 window.kleiderordnung = {
-    state: {
-        /** @type {HTMLElement|null} */ rootElement: null,
-        /** @type {HTMLElement|null} */ menu: null,
-        /** @type {HTMLElement|null} */ menuOpenButton: null,
-        /** type {number[]} */ observableTimeoutsByTargetElementId: [],
-        /** @type {number} */ genericIdCounter: 0,
-        /** @type {number} */ currentAnimationReplayTimeoutIdNr: 0,
-        /** @type {object|null} LottiePlayer instance */ introAnimation: null,
-        /** @type {boolean} */ isHeaderStuck: false,
+  state: {
+    /** @type {number} */                    currentAnimationReplayTimeoutIdNr: 0,
+    /** @type {number} */                    genericIdCounter: 0,
+    /** @type {object|null} LottiePlayer */  introAnimation: null,
+    /** @type {boolean} */                   isHeaderStuck: false,
+    /** @type {HTMLElement|null} */          menu: null,
+    /** @type {HTMLElement|null} */          menuOpenButton: null,
+    /** type {number[]} */                   observableTimeoutsByTargetElementId: [],
+    /** @type {HTMLElement|null} */          rootElement: null,
+  },
+  observers: {
+    /** @type {IntersectionObserver|null} */ stickyHeaderObserver: null,
+    /** @type {IntersectionObserver|null} */ waypointObserver: null,
+  },
+  config: {
+    allowableElementsClassName: 'allowable--on-visibility', // triggers consent before loading external content
+    feedCookieKey: 'instafeed', // name of the cookie set to remember consent
+    feedCookieValue: 'allow', // default value
+    feedCookieMaxAgeSeconds: 31536000, // 1 year
+    feedSectionClassName: 'socialmedia__feed__section',
+    feedSectionActiveClassName: 'socialmedia__feed__section--active',
+    feedContainerClassName: 'socialmedia__feed__container',
+    feedContainerActiveClassName: 'socialmedia__feed__container--active', // set to feed container when allowed
+    feedDataKey: 'allow',                // data key to control the expiry / validity when clicking allow
+    feedDataValueAlways: 'always',      // data value to always allow (as opposed to the session default)
+    feedScriptUrlDataKey: 'scripturl', // data key which JavaScript to load (absolute or relative to project)
+    feedStyleUrlDataKey: 'styleurl', // data key which stylesheet to load (absolute or relative to project)
+    introKeyvisualAnimationId: 'intro-keyvisual-animation',
+    introKeyvisualMousetrapId: 'intro-keyvisual-mousetrap',
+    introKeyvisualTimeoutMilliseconds: 30000,
+    introKeyvisualScriptId: 'lottie-player-script',
+    introKeyvisualComponentTagName: 'lottie-player',
+    mainMenuNavWrapperSelector: 'navigation--main-navigation', // select parent menu from descendant button handler
+    menuId: 'primary-menu',
+    menuCloseButtonClassName: 'navigation__toggle--close',
+    menuOpenButtonClassName: 'navigation__toggle--open',
+    menuOpenedClassName: 'navigation__menu-opened', // .target for progressive enhancement of :target
+    menuLinksSelector: '#primary-menu a[href]',    // links inside the menu
+    noGlobalThisClassName: 'no-globalthis',       // set if special intro animation fallback necessary
+    /** @object IntersectionObserver options */
+    observerOptions: {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
     },
-    observers: {
-        stickyHeaderObserver: null,
-        waypointObserver: null,
-    },
-    config: {
-        allowableElementsClassName: 'allowable--on-visibility', // triggers consent challenge before external content loading
-        removableButtonsClassName: 'button__remove',
-        feedCookieKey: 'instafeed', // name of the cookie set to remember consent
-        feedCookieValue: 'allow', // default value
-        feedCookieMaxAgeSeconds: 31536000, // 1 year
-        feedSectionClassName: 'socialmedia__feed__section',
-        feedSectionActiveClassName: 'socialmedia__feed__section--active',
-        feedContainerClassName: 'socialmedia__feed__container',
-        feedContainerActiveClassName: 'socialmedia__feed__container--active', // set to feed container when allowed and loaded
-        feedDataKey: 'allow', // data key to control the expiry / validity when clicking allow
-        feedDataValueAlways: 'always', // data value to always allow (as opposed to the session default)
-        feedScriptUrlDataKey: 'scripturl', // data key which JavaScript to load (absolute or relative to project)
-        feedStyleUrlDataKey: 'styleurl', // data key which stylesheet to load (absolute or relative to project)
-        menuId: 'primary-menu',
-        menuCloseButtonClassName: 'navigation__toggle--close',
-        menuOpenButtonClassName: 'navigation__toggle--open',
-        menuOpenedClassName: 'navigation__menu-opened', // .target for progressive enhancement of :target
-        menuLinksSelector: '#primary-menu a[href]', // links inside the menu
-        mainMenuNavWrapperSelector: 'navigation--main-navigation', // select parent menu from descendant button handler
-
-        waypointsQuerySelector: '#menu-main a.menu-item-link-waypoint',
-        socialMediaSectionId: 'socialmedia',
-
-        stickyHeaderId: 'site-header',
-        stuckClassName: 'stuck', // emulate ::stuck pseudo class for sticky header styling
-
-        introKeyvisualAnimationId: 'intro-keyvisual-animation',
-        introKeyvisualMousetrapId: 'intro-keyvisual-mousetrap',
-        introKeyvisualTimeoutMilliseconds: 30000,
-        introKeyvisualScriptId: 'lottie-player-script',
-        introKeyvisualComponentTagName: 'lottie-player',
-        noGlobalThisClassName: 'no-globalthis',
-
-        supportsIntersectionObserver: (typeof IntersectionObserver === 'function'),
-        supportsGlobalThis: (typeof globalThis === 'object'),
-
-        /** @object IntersectionObserver options */
-        observerOptions: {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.1
-        }
-    }
+    removableButtonsClassName: 'button__remove', // elements to be removed by consent handling
+    socialMediaSectionId: 'socialmedia',
+    stickyHeaderId: 'site-header',
+    stuckClassName: 'stuck', // emulate ::stuck pseudo class for sticky header styling
+    stickyHeaderHeightProperty: '--header-height',
+    stickyStuckHeaderHeightProperty: '--header-height--stuck',
+    supportsGlobalThis: (typeof globalThis === 'object'),
+    supportsIntersectionObserver: (typeof IntersectionObserver === 'function'),
+    waypointsQuerySelector: '#menu-main a.menu-item-link-waypoint',
+  }
 }
 
 if (typeof document.querySelector === 'function') {
-    // used to update actual header height to fix anchor positions
-    window.kleiderordnung.state.rootElement = document.querySelector(':root');
+  // used to update actual header height to fix anchor positions
+  window.kleiderordnung.state.rootElement = document.querySelector(':root');
 }
 
-
 document.addEventListener('DOMContentLoaded', function () {
+  /* body.no-js should be handled by WordPress core in php body_class() */
+  /* workaround against unreliable default fallback class on custom front page */
+  document.body.classList.remove('no-js');
+  document.body.classList.add('js');
 
-    /* body.no-js should be handled by WordPress core in php body_class() */
-    /* workaround against unreliable default fallback class on custom front page */
-    document.body.classList.remove('no-js');
-    document.body.classList.add('js');
+  if (typeof window.matchMedia === 'function') {
+    var isMobileQuery = window.matchMedia('(max-width: 768px)');
+    window.kleiderordnung.config.isMobile = (isMobileQuery.matches);
 
-    if (typeof window.matchMedia === 'function') {
-        var isMobileQuery = window.matchMedia('(max-width: 768px)');
-        window.kleiderordnung.config.isMobile = (isMobileQuery.matches);
+    var prefersReducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+    window.kleiderordnung.config.prefersReducedMotion = (prefersReducedMotionQuery.matches);
 
-        var prefersReducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-        window.kleiderordnung.config.prefersReducedMotion = (prefersReducedMotionQuery.matches);
+    var prefersMoreContrastQuery = window.matchMedia('(prefers-contrast: more)');
+    window.kleiderordnung.config.prefersMoreContrast = (prefersMoreContrastQuery.matches);
+  }
 
-        var prefersMoreContrastQuery = window.matchMedia('(prefers-contrast: more)');
-        window.kleiderordnung.config.prefersMoreContrast = (prefersMoreContrastQuery.matches);
-    }
+  kleiderordnung_activateIntroAnimation(window.kleiderordnung.config);
+  kleiderordnung_observeStickyHeader(window.kleiderordnung.config);
+  kleiderordnung_observeWaypointAnchors(window.kleiderordnung.config);
+  kleiderordnung_enhanceNavigationMenu(window.kleiderordnung.config);
+  kleiderordnung_handleElementsActivatedOnVisibilityAndConsent(
+    kleiderordnung_intersectionCallback,
+    window.kleiderordnung.config
+  );
+  kleiderordnung_carouselSetup();
+  kleiderordnung_keepAnchorTargetInLanguageSwitcher();
+  kleiderordnung_decoratedParagraphsHeightAdjustment(kleiderordnung.rootElement);
+  kleiderordnung_activateAnalyticsTracking();
 
-    kleiderordnung_activateIntroAnimation(window.kleiderordnung.config);
-    kleiderordnung_observeStickyHeader();
-    kleiderordnung_observeWaypointAnchors(window.kleiderordnung.config);
-
-    kleiderordnung_enhanceNavigationMenu(window.kleiderordnung.config);
-
-    kleiderordnung_handleElementsActivatedOnVisibilityAndConsent(kleiderordnung_intersectionCallback, window.kleiderordnung.config);
-    kleiderordnung_carouselSetup();
-    kleiderordnung_keepAnchorTargetInLanguageSwitcher();
-    kleiderordnung_decoratedParagraphsHeightAdjustment(kleiderordnung.rootElement);
-    kleiderordnung_activateAnalyticsTracking();
-
-    window.setTimeout(kleiderordnung_activateLazyLoadedMailtoLinks, 9000);
+  window.setTimeout(kleiderordnung_activateLazyLoadedMailtoLinks, 9000);
 });
