d0996ae52afa494b715e086c9554c736149de1b5
Ingo Steinke <ingosteinke@gmx.de>
2023-08-19 23:04:38 +0200

    target version 2.8.0

--- mu-plugins/kleiderordnung-dashboard-widget.php
+++ mu-plugins/kleiderordnung-dashboard-widget.php
@@ -2,10 +2,10 @@
 /**
  * @package KleiderOrdnung
  * @author openmindculture
- * @version 2.7.0
+ * @version 2.8.0
  *
  * @wordpress-plugin
- * Version: 2.7.0
+ * Version: 2.8.0
  * Tested up to: 6.3
  * Plugin Name: KleiderOrdnung Dashboard Widget
  * Text Domain: kleiderOrdnung-dashboard-widget
