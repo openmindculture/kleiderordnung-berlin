85243df1e9160118a793f1943e7b70b3dc8bc4c2
Ingo Steinke <ingosteinke@gmx.de>
2023-08-19 16:48:20 +0200

    debug loading resources on demand

--- src/js/inc/fetchAndAppendStyleFromFile.js
+++ src/js/inc/fetchAndAppendStyleFromFile.js
@@ -4,7 +4,11 @@ import {kleiderordnung_verifyAllowedUrl} from './verifyAllowedUrl';
  *  @property {number} feedStyleFetchRetryTimeoutMilliseconds
  */
 export function kleiderordnung_fetchAndAppendStyleFromFile(styleFileUrl, config) {
+  console.log('kleiderordnung_fetchAndAppendStyleFromFile');
+  console.log('styleFileUrl ' + styleFileUrl);
+  console.log('config ', config);
   if (!config || !styleFileUrl || !kleiderordnung_verifyAllowedUrl(styleFileUrl)) { return; }
+  console.log('try to fetch styleFileUrl ' + styleFileUrl);
   fetch(styleFileUrl)
     .then(function(response) {
       return response.text();
@@ -12,7 +16,9 @@ export function kleiderordnung_fetchAndAppendStyleFromFile(styleFileUrl, config)
     .then(function(text) {
       var style = document.head.appendChild(document.createElement('style'));
       style.textContent = text;
+      console.log('done: applied style from file');
     }).catch(function(err) {
+      console.log('failed to load or apply style from file');
       if (window.kleiderordnung.state.feedStyleFetchRetryCount < config.feedStyleMaxFetchRetries) {
         window.kleiderordnung.state.feedStyleFetchRetryCount++;
         window.setTimeout(config.feedStyleFetchRetryTimeoutMilliseconds, function(){
--- src/js/inc/verifyAllowedUrl.js
+++ src/js/inc/verifyAllowedUrl.js
@@ -2,17 +2,21 @@
  * @param {string} urlToVerify
  */
 export function kleiderordnung_verifyAllowedUrl(urlToVerify) {
+  console.log('urlToVerify' + urlToVerify);
   var feedResourcesAllowedAbsoluteUrls = [
     'https://assets.juicer.io/embed.js'
   ];
   for (var i=0; i < feedResourcesAllowedAbsoluteUrls.length; i++) {
     if (urlToVerify === feedResourcesAllowedAbsoluteUrls[i]) {
+      console.log('allowed as feedResourcesAllowedAbsoluteUrl');
       return true;
     }
   }
   var sameUrlPrefix = window.location.protocol + '//' + window.location.host + '/wp-content/themes/kleiderordnung/';
   if (urlToVerify.indexOf(sameUrlPrefix) === 0) {
+    console.log('allowed by sameUrlPrefix');
     return true;
   }
+  console.log('disallowed! sameUrlPrefix: ' + sameUrlPrefix);
   return false;
 }
