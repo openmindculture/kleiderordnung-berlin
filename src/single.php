ac3b177b707f661520b5b2b498ff5e209488e44c
Ingo Steinke <ingosteinke@gmx.de>
2023-08-25 18:19:58 +0200

    single news page

--- src/single.php
+++ src/single.php
@@ -3,28 +3,42 @@
 <?php define('KLEIDERORDNUNG_PAGE_TITLE', esc_html(get_the_title())) ?>
 <?php include( KLEIDERORDNUNG_DIR . '/inc/structure/html-head.php') ?>
 <body <?php body_class(); ?> itemtype="https://schema.org/WebPage" itemscope>
-<!--
-     single.php                         (should handle new and any post type that has no own file)
-     single-offer.php
-     single-story.php                   (currently not needed but probably better to define)
-     category.php                       (default taxonomy archive, should handle news overview)
-     taxonomy-offer_section.php         (we could discriminate based on if it has an image etc.)
-     taxonomy-offer_section-angebot.php (if we really want to discriminate based on category)
-     taxonomy-offer_section-weitere.php
--->
-<hr><pre>emitted by single.php</pre><hr><!-- TODO remove debug output -->
 <?php
   if (have_posts()) :
     while (have_posts()) :
       the_post();
 
       include( KLEIDERORDNUNG_DIR . '/inc/structure/header.php');
-      echo '<main id="wp--skip-link--target">';
-      ?>
-      <h1 class="page-title" id="content"><?php the_title(); ?></h1>
-      <?php
-      the_content();
-      echo '</main>';
+      ?><main id="wp--skip-link--target">';
+      <section class="page__main__section">
+        <div class="page__main__entry">
+
+        <article class="news__post news__post--single">
+          <figure class="news__post__image">
+            <picture>
+              <?php the_post_thumbnail() ?>
+            </picture>
+          </figure>
+          <div class="news__post__content">
+            <div class="news__post__title">
+              <?php
+              $category = get_the_category();
+              if($category && $category[0]) {
+                echo $category[0]->cat_name;
+              }
+              ?>
+            </div>
+            <div class="news__post__text">
+              <h1><?php the_title() ?></h1>
+              <p><?php the_content() ?></p>
+            </div>
+          </div>
+        </article>
+
+          </div>
+        </section>
+      </main>
+<?php
       include( KLEIDERORDNUNG_DIR . '/inc/structure/footer.php');
       include( KLEIDERORDNUNG_DIR . '/inc/structure/admin-edit-link.php');
 
