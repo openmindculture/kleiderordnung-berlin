7870d73a74123a98fdd5142ec27b80ff6e1584fa
Ingo Steinke <ingosteinke@gmx.de>
2023-08-27 20:21:34 +0200

    fix static pages

--- src/page.php
+++ src/page.php
@@ -7,6 +7,7 @@ include( KLEIDERORDNUNG_DIR . '/inc/structure/html-head.php');
 <body <?php body_class(); ?> itemtype="https://schema.org/WebPage" itemscope>
 <?php
 include( KLEIDERORDNUNG_DIR . '/inc/structure/header.php');
+if (get_post_type() === 'post') :
 ?><main id="wp--skip-link--target">
   <section id="page__main__section">
     <div class="page__main__entry">
@@ -18,6 +19,23 @@ include( KLEIDERORDNUNG_DIR . '/inc/structure/header.php');
   </section>
 </main>
 <?php
+else:
+  if (have_posts()) :
+?>
+<main id="wp--skip-link--target">
+  <?php
+    while (have_posts()) :
+      the_post();
+  ?>
+      <h1 class="page-title" id="content"><?php the_title(); ?></h1>
+  <?php
+      the_content();
+    endwhile;
+  ?>
+</main>
+<?php
+  endif; // have_posts
+endif; // is_single && get_post_type post or else iterate the_content
 include( KLEIDERORDNUNG_DIR . '/inc/structure/footer.php');
 include( KLEIDERORDNUNG_DIR . '/inc/structure/admin-edit-link.php');
 ?>
