98dcc4f7d877eefcb9ab8cdb259dbae0ef37cd09
Ingo Steinke <ingosteinke@gmx.de>
2023-08-30 10:13:36 +0200

    fix footer link handler

--- src/js/inc/handleFooterDetailLink.js
+++ src/js/inc/handleFooterDetailLink.js
@@ -2,7 +2,7 @@
 
 /* global document */
 var kleiderordnung_handleFooterDetailLink = function() {
-  var footerEcofriendlyInformation = document.getElementBy('footer__ecofriendly');
+  var footerEcofriendlyInformation = document.getElementById('footer__ecofriendly');
   var footerDetailLink = document.getElementById('footer__link--ecofriendly');
   if (!footerEcofriendlyInformation || !footerDetailLink) { return; }
   footerDetailLink.addEventListener('click', function (event) {
