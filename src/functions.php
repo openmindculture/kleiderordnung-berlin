958a1fafefd7aa070e14e9ecfbd864f757dc2fe5
Ingo Steinke <ingosteinke@gmx.de>
2023-09-11 08:34:04 +0200

    theme target version 2.9.3

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.9.2' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '2.9.3' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
