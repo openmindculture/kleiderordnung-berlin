148c601faff59c300815cbff0191ad1254b685fe
Ingo Steinke <ingosteinke@gmx.de>
2023-09-19 12:38:01 +0200

    long testimonial text layout #46

--- src/inc/structure/stories-testimonial-item.php
+++ src/inc/structure/stories-testimonial-item.php
@@ -8,6 +8,8 @@
 if (!isset($loop_index)) { $loop_index = 0; }
 if (!isset($last_index)) { $last_index = 0; }
 $has_thumbnail = (isset($resorted_post_id)) ? has_post_thumbnail($resorted_post_id) : has_post_thumbnail();
+$current_title = isset($resorted_post_id) ? get_the_title($resorted_post_id) : get_the_title();
+$current_content = isset($resorted_post_id) ? get_the_content($resorted_post_id) : get_the_content();
 ?>
 <div class="stories__story carousel__item carousel__item--index-<?php echo $loop_index?>"<?php
   if ($loop_index == 0) {
@@ -23,12 +25,16 @@ $has_thumbnail = (isset($resorted_post_id)) ? has_post_thumbnail($resorted_post_
   ?>">
     <?php if (isset($resorted_post_id)) { echo get_the_post_thumbnail($resorted_post_id); } else { the_post_thumbnail(); } ?>
   </figure>
-  <figure class="stories__story__quote">
+  <figure class="stories__story__quote<?php
+  if (strlen($current_content) > 360) {
+    echo ' long';
+  }
+  ?>">
     <figcaption class="stories__story__quote__author">
-      <?php if (isset($resorted_post_id)) { echo get_the_title($resorted_post_id); } else { the_title(); } ?>
+      <?php if (isset($resorted_post_id)) { echo $current_content; ?>
     </figcaption>
     <blockquote class="stories__story__quote__text">
-      <?php if (isset($resorted_post_id)) { echo get_the_content($resorted_post_id); } else { the_content(); } ?>
+      <?php if (isset($resorted_post_id)) { echo $current_content; ?>
     </blockquote>
   </figure>
 </div>
