cc41ff366a3775b7e0bd0c68ace4938377c72c0b
Ingo Steinke <ingosteinke@gmx.de>
2023-09-26 20:31:59 +0200

    footer layout, link hover fixup #57

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.1.2' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.1.3' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
