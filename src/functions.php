ce53653de861139a0fe0ec3a7a7ea51fd99dd3c4
Ingo Steinke <ingosteinke@gmx.de>
2023-10-20 21:35:51 +0200

    harmonize footer logo sizes

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.1.7' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.1.8' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
