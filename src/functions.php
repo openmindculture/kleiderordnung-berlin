f22f6c53fcf6f2eff7c8d4358c2a17ed9d46d335
Ingo Steinke <ingosteinke@gmx.de>
2023-11-14 12:17:14 +0100

    release 3.2.3

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.2.2' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.2.3' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
