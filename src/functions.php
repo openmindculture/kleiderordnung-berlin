7ae2448a03c4d36625c43964bc0413b4ba074e2f
Ingo Steinke <ingosteinke@gmx.de>
2023-12-08 16:15:19 +0100

    theme version 3.3.1

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.3.0' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.3.1' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
