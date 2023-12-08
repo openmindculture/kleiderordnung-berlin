1e9362dab636832a7d28c5bf8172ac304f59e06c
Ingo Steinke <ingosteinke@gmx.de>
2023-12-08 15:30:52 +0100

    calendly link, code cleanup #78

--- src/inc/structure/contact-calendar-popup.php
+++ src/inc/structure/contact-calendar-popup.php
@@ -12,7 +12,7 @@
     <div class="calendly-close-overlay"></div>
     <div class="calendly-popup">
       <h2><?php _e( 'Buche einen Termin', 'kleiderordnung' ) ?></h2>
-      <div class="calendly-popup-content" data-url="?><?php echo KLEIDERORDNUNG_CALENDY_URI ?>/30min">
+      <div class="calendly-popup-content" data-url="?><?php echo KLEIDERORDNUNG_CALENDY_URI ?>/15-minute-meeting">
         <div
           id="socialmedia-feed-consent-calendly"
           class="socialmedia__feed__consent feed__consent socialmedia__feed__consent--calendly"
@@ -44,7 +44,7 @@
         </div>
         <iframe
           src="about:blank"
-          data-src="<?php echo KLEIDERORDNUNG_CALENDY_URI ?>/30min?embed_domain=<?php
+          data-src="<?php echo KLEIDERORDNUNG_CALENDY_URI ?>/15-minute-meeting?embed_domain=<?php
           $urlparts = explode('/', get_site_url());
           echo urlencode($urlparts[2]);
 ?>&amp;embed_type=PopupText"
