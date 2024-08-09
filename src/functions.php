361d12e4320dfafc40ce24a98db901673e9e7082
Ingo Steinke <ingosteinke@gmx.de>
2024-08-09 17:29:24 +0200

    fix layer order for older iOS

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_THEME_VERSION' ) ) {
-  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.7.0' );
+  define( 'KLEIDERORDNUNG_THEME_VERSION', '3.7.1' );
 }
 
 if ( ! defined( 'KLEIDERORDNUNG_DIR' ) ) {
